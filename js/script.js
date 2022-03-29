3772995b1ff6e28756e7ab63e998a5f529add30a
Fabio-F16 <fipsfabio95@gmail.com>
2022-03-29 16:00:36 +0200

    funzionante

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
-console.log(contacts)
-console.log('js ok')
+console.log(contacts);
+console.log('js ok');
+
+
+const app = new Vue({
+
+    el: '#root',
+    data: {
+        contacts,
+    }
+
+})
