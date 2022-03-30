07f8709c03f134c5f503df63be3aa65ef7bca561
Fabio-F16 <fipsfabio95@gmail.com>
2022-03-30 15:47:31 +0200

    Milestone 2 e 3

--- js/script.js
+++ js/script.js
@@ -7,6 +7,34 @@ const app = new Vue({
     el: '#root',
     data: {
         contacts,
+        contactClicked: undefined,
+        newMessage: '',
+
+    },
+    methods: {
+        setContactClicked(i) {
+            this.contactClicked = i;
+            console.log('click');
+        },
+        typeOfMessage(status) {
+            if (status === 'sent') {
+                return 'send'
+            } else {
+                return 'received'
+            }
+        },
+        sendMessage() {
+            const newMessageToSend = {
+                date: '',
+                message: '',
+                status: 'sent'
+            };
+
+            newMessageToSend.message = this.newMessage;
+            this.contacts[this.contactClicked].messages.push(newMessageToSend);
+
+            this.newMessage = '';
+        }
     }
 
 })
